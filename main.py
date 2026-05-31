a86a7669dc56693e096e853a8d8c498956724ea4
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 06:28:52 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class helperDtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = helperDtringifyWorker()
+    print("Worker engine initialized.")
